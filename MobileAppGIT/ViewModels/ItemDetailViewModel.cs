f0f819a64220314172d8be9cdb5db1ca87f20f3d
Akeem Williams <akeem.d.williams@outlook.com>
2020-05-04 20:48:34 -0400

    fix folsers not sent. first commit

---
+++ MobileAppGIT/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using MobileAppGIT.Models;
+
+namespace MobileAppGIT.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
